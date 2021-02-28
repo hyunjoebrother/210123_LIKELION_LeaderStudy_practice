eebbed11fe2e7fbc500d137f08c6fd6c02f2bf25
hyunjoebrother <hyunjoe31@gmail.com>
2021-02-28 18:34:47 +0900

    210228_LIKELION_study

---
+++ templates/inputprint.py
@@ -0,0 +1,29 @@
+## 출력 함수 print
+# 실행 터미널 명령어 $python (파일이름).py 
+# 실행하기 전에 코드 저장하고!
+
+#1+2
+#print(1+2)
+
+# 문자열도 출력 가능
+#print("아주NICE")
+
+## 변수 출력
+# 변수는 변할 여지가 있는 data므로 변수 '이름'이 필요하다
+# 변수에 값 지정하기 : 변수 이름 = 변수 안에 담고 싶은 값
+#number = 16
+#print(number + 1)
+
+#leader = "최승철"
+#leader = input("반장의 이름을 입력하세요") # 프로프트 : 입력받을 때 뜨는 메시지
+#print("우리반 반장 이름은", leader)
+
+#number1 = input("첫번째 숫자를 입력하세요 : ")
+#number2 = input("두번째 숫자를 입력하세여 : ")
+
+number1 = int(input("첫번째 숫자를 입력하세요 : "))
+number2 = int(input("두번째 숫자를 입력하세요 : "))
+print(number1 + number2)
+print(number2)
+
+
---
+++ templates/numberdata.py
@@ -0,0 +1,14 @@
+## 자료형 - 숫자형
+# 정수형(int) & 실수형(float)
+
+num1 = int(input("첫번째 정수를 입력하세요 : "))
+num2 = int(input("두번째 정수를 입력하세요 : "))
+
+print(num1+num2) #덧셈
+print(num1*num2) #곱셈
+
+print(num1//num2) #몫
+print(num1%num2) #나머지
+print(num1/num2) #나눗셈
+
+print(num1**num2) #거듭제곱
